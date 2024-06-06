d8f5a196000fb58729925ffdda4e47c7d7ba6f9c
matshidisolp <mpitswane@gmail.com>
2024-06-06 20:11:29 +0700

    3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,9 +10,9 @@ void binary_tree_delete(binary_tree_t *tree)
 		return;
 
     /* Delete left & right recursively */
-    binary_tree_delete(tree->left);
-    binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 
     /* Delete the current node */
-    free(tree);
+	free(tree);
 }
